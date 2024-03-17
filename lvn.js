389a27ec6df58c5662053ce89abb334d7c5832ec
Abukarsh214 <156728366+Abukarsh214@users.noreply.github.com>
2024-03-17 00:03:22 -0500

    Add files via upload

---
+++ lvn.js
@@ -0,0 +1,12 @@
+const modal = document.querySelector('#modal');
+const openModal = document.querySelector('.open-button');
+const closeModal = document.querySelector('.close-button');
+
+openModal.addEventListener('click', () => {
+    modal.show();
+});
+
+
+closeModal.addEventListener('click', () => {
+    modal.close();
+});
